affea55e622d75c837b7ddf2fd41c93413a3e5c5
walterswoodward <walterwoodward.dev@gmail.com>
2024-02-09 16:52:39 -0800

    removes opening paragraph from portfolio page

--- src/pages/portfolio/portfolio.tsx
+++ src/pages/portfolio/portfolio.tsx
@@ -23,20 +23,7 @@ const Portfolio = () => {
   const { data, error } = useSWR("/api/profile-data", fetcher);
 
   if (error) return <div>Failed to load</div>;
-  if (!data)
-    return (
-      <Page
-        className="items-center pb-16 font-alegreya pt-[50px]"
-        title="Portfolio"
-      >
-        <div className="p-[10px] text-md pb-[20px]">
-          Unfortunately since most of the projects I have worked on have been
-          for proprietary products, the only example I currently have of my work
-          dates back to the beginning of my journey with HandicappedPets and the
-          SureFit Calculator.
-        </div>
-      </Page>
-    );
+  if (!data) return <></>;
 
   const projects: ProjectData = [...data];
 
@@ -59,16 +46,7 @@ const Portfolio = () => {
       className="items-center pb-16 font-alegreya pt-[50px]"
       title="Portfolio"
     >
-      <div className="p-[10px] text-md pb-[20px]">
-        Unfortunately since most of the projects I have worked on have been for
-        proprietary products, the only example I currently have of my work dates
-        back to the beginning of my journey with HandicappedPets and the SureFit
-        Calculator.
-      </div>
-
-      {data && (
-        <div className="flex justify-between flex-col sm:flex-row">{cards}</div>
-      )}
+      <div className="flex justify-between flex-col sm:flex-row">{cards}</div>
     </Page>
   );
 };
