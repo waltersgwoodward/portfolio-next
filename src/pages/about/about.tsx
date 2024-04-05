0a357f7adff1e6c6c378a6f5aab83e4639f4ec0e
walterswoodward <walterwoodward.dev@gmail.com>
2024-04-05 10:18:29 -0700

    updates/simplifies about page

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,4 +1,5 @@
 import Page from "@/components/page";
+import { Link } from "@chakra-ui/next-js";
 
 const About = () => {
   return (
@@ -9,29 +10,31 @@ const About = () => {
     >
       <div className="max-w-[900px]">
         <div className="p-[10px] text-md">
-          Hi there! My name is Walter Woodward and welcome to my portfolio
-          website. It was built with NextJS and deployed using AWS Amplify.
+          Hi there! My name is Walter Woodward this is a simply portfolio
+          website I built earlier this year.
         </div>
         <div className="p-[10px] text-md">
           I am a Full Stack Software Developer currently based out of Oregon
-          City, OR. At the moment I am primarily building with the following
-          tools/stack: TypeScript, JavaScript, React, NextJS, NestJS, GraphQL,
-          and PostgreSQL.
-        </div>
-        <div className="p-[10px] text-md">
-          I got into software development for three primary reasons: firstly,
-          because I enjoy it; secondly, as my work experience demonstrates,
-          because I have an aptitude for it; and lastly because I have a desire
-          to contribute to making the world a better place through the use of
-          technology. Having established myself over the past five years as
-          a software developer, I am now actively seeking/considering remote
-          positions for mission-driven companies based in the United States.
-          Please reach out if you have a relevant position that you think I
-          would be a good fit for. Thank You 🙏 🙇‍♂️
-        </div>
-        <div className="p-[10px] text-md italic">
-          “But seek first the kingdom of God and his righteousness, and all
-          these things will be added to you.” - Matthew 6:33 ESV
+          City, OR. If you are interested in hiring me for a position, please
+          contact me using my{" "}
+          <Link
+            className="page-row__link font-medium"
+            href="/contact"
+            aria-label="Click here to view Walter's Contact form"
+          >
+            <span className="text-blue-600 hover:underline">contact page</span>
+          </Link>
+          , or via my{" "}
+          <Link
+            href="https://www.linkedin.com/in/walter-woodward-83b656130/"
+            className="page-row__link font-medium"
+            aria-label="Click here to view Walter's LinkedIn Profile"
+          >
+            <span className="text-blue-600 hover:underline">
+              LinkedIn profile
+            </span>
+          </Link>
+          . Thank You 🙏
         </div>
       </div>
        
